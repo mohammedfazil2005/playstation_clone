68f7f185b91fe13edf4923b274a353d3dc3b8171
Mohammedfazil <mohammedfazil20005@gmail.com>
2025-01-27 20:14:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/navbar/Navbar'
+import Heading from './Components/homeHeading/Heading'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Heading/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/homeHeading/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Heading.css'
+const Heading = () => {
+  return (
+    <div>
+ 
+    </div>
+  )
+}
+
+export default Heading
