0e8f5668c8f3b0a16f6f416e23a4b52ad9e9cf1f
Mohammedfazil <mohammedfazil20005@gmail.com>
2025-02-01 12:38:59 +0530

    video added

--- src/Components/psPlus/PsPlus.jsx
+++ src/Components/psPlus/PsPlus.jsx
@@ -1,16 +1,43 @@
 import React from 'react'
 import './PsPlus.css'
+import ps5Adp from '../../assets/ps5Adp.mp4'
+import ps5adp2 from '../../assets/ps5adp2.mp4'
 function PsPlus() {
     return (
-        <div className='plus-pa'>
-            <div className="plus-parent">
-                <div className="heading-plus">
-                    <img src="https://st1.latestly.com/wp-content/uploads/2024/11/9-158-2.jpg" alt="" />
-                    <h1>Discover PlayStation Plus</h1>
-                    <p>Play hundreds of incredible PS5, PS4 and classic PlayStation games, and discover epic adventures, unique indies, family favourites, and everything in between.</p>
+        <div className='ps5-pa'>
+            <div className="ps5-parent">
+                <div className="ps5-heading">
+                    <h1>Immersive play with the <br /> DualSense® wireless <br />controller</h1>
+                   <h6>Discover a deeper gaming experience* with the incredibly tactile features of the innovative PS5 controller.</h6>
+                   <p>*Compared to DUALSHOCK@4 wireless controller</p>
                 </div>
-                <div className="plus-ad-parent">
-                    <img src="https://direct.playstation.com/content/dam/ps-direct/us/en/marketing/images/fy23/wk32-pulse-elite-and-explore/pulse_elite_explore_top_nav_470x264.jpg" alt="" />
+                <div className="ps5-ad-parent">
+                   <div>
+                    <video src={ps5Adp} autoPlay loop muted></video>
+                    <div className='ps5-details'>
+                        <div>
+                            <img src="https://www.beamng.com/attachments/upload_2023-10-22_4-32-3-png.1072323/" alt="" />
+                        </div>
+                        <div>
+                        <h2>Adaptive Triggers1</h2>
+                            <p>Dynamic resistance mimics the tension of interactions with in-game gear and objects in select PS5 games.</p>
+                            <p>From pulling back an increasingly tight bowstring to hitting the brakes on a speeding car, feel physically connected to your on-screen actions.</p>
+                        </div>
+                    </div>
+                   </div>
+                   <div>
+                    <video src={ps5adp2} autoPlay loop muted></video>
+                    <div className='ps5-details'>
+                        <div>
+                            <img src="https://www.gamestore.com.kw/web/image/77984/adaptive-triggers-icon-03-en-15jun20.png?access_token=274d7902-87a4-4467-9a18-bad75b02f875" alt="" />
+                        </div>
+                        <div>
+                            <h2>Haptic Feedback1</h2>
+                            <p>Responsive vibrations react to your in-game choices and simulate environmental factors in select PS5 games.  </p>
+                            <p>In your hands, these dynamic vibrations can simulate the feeling of everything from environments to the recoil of different weapons.</p>
+                        </div>
+                    </div>
+                   </div>
                 </div>
             </div>
         </div>
